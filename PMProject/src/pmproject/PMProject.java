d551e5a846753a9958b7376decc558ec8e30a0dc
jasonyu0705 <49047194+jasonyu0705@users.noreply.github.com>
2022-05-10 13:56:26 -0400

    hello

---
+++ PMProject/src/pmproject/PMProject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pmproject;
+
+/**
+ *
+ * @author jason
+ */
+public class PMProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("final tester");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello");
+        }
+    }
+    
+}

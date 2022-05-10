80f06351de46f1c4a5469e07d2ce1b37385fee63
jasonyu0705 <49047194+jasonyu0705@users.noreply.github.com>
2022-05-10 18:12:58 -0400

    done question class i think

--- PMProject/src/pmproject/Question.java
+++ PMProject/src/pmproject/Question.java
@@ -10,7 +10,6 @@ package pmproject;
  */
 public class Question {
 
-
     private String question;
     private String option1;
     private String option2;
@@ -36,42 +35,66 @@ public class Question {
         option4 = o4;
         answer = a;
     }
-    
-    public String getQuestion(){
+
+    public String getQuestion() {
         return question;
     }
-    
-    public String getOption(int n){
-        if (n == 1){
+
+    public String getOption(int n) {
+        if (n == 1) {
             return option1;
-        }else if (n==2){
+        } else if (n == 2) {
             return option2;
-        }else if (n==3){
+        } else if (n == 3) {
             return option3;
-        }else{
+        } else {
             return option4;
         }
     }
-    public int getAnswer(){
+
+    public int getAnswer() {
         return answer;
     }
-    public void setQuestion(String q){
-        this.question=q;
+
+    public void setQuestion(String q) {
+        this.question = q;
     }
-    public void setOption(String o, int n){
-        if (n == 1){
-            this.option1=o;
-        }else if (n==2){
-            this.option2=o;
-        }else if (n==3){
-            this.option3=o;
-        }else{
-            this.option4=o;
+
+    public void setOption(String o, int n) {
+        if (n == 1) {
+            this.option1 = o;
+        } else if (n == 2) {
+            this.option2 = o;
+        } else if (n == 3) {
+            this.option3 = o;
+        } else {
+            this.option4 = o;
         }
     }
-    public void setAnswer(int n){
-        this.answer=n;
+
+    public void setAnswer(int n) {
+        this.answer = n;
+    }
+
+    public String toString() {
+        return question + "?" + "\n"
+                + " a) " + option1
+                + "\n b) " + option2
+                + "\n c) " + option3
+                + "\n d) " + option1;
+    }
+
+    public Question clone() {
+        Question q = new Question(question, option1, option2, option3, option4, answer);
+        return q;
+    }
+
+    public boolean equals(Question q) {
+        if (this.question.equals(q)) {
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
-
